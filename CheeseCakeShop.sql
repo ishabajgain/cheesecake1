54f6603850c4714465477ccd34efe8604cff7a3b
Manish B.C <manysh23@gmail.com>
2022-05-22 07:23:28 +0545

    added new sql

---
+++ CheeseCakeShop.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2022 at 03:37 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `CheeseCakeShop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+-- Error reading structure for table CheeseCakeShop.cart: #1932 - Table 'cheesecakeshop.cart' doesn't exist in engine
+-- Error reading data for table CheeseCakeShop.cart: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `CheeseCakeShop`.`cart`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+-- Error reading structure for table CheeseCakeShop.categories: #1932 - Table 'cheesecakeshop.categories' doesn't exist in engine
+-- Error reading data for table CheeseCakeShop.categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `CheeseCakeShop`.`categories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+-- Error reading structure for table CheeseCakeShop.contacts: #1932 - Table 'cheesecakeshop.contacts' doesn't exist in engine
+-- Error reading data for table CheeseCakeShop.contacts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `CheeseCakeShop`.`contacts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+-- Error reading structure for table CheeseCakeShop.orders: #1932 - Table 'cheesecakeshop.orders' doesn't exist in engine
+-- Error reading data for table CheeseCakeShop.orders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `CheeseCakeShop`.`orders`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+-- Error reading structure for table CheeseCakeShop.payments: #1932 - Table 'cheesecakeshop.payments' doesn't exist in engine
+-- Error reading data for table CheeseCakeShop.payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `CheeseCakeShop`.`payments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+-- Error reading structure for table CheeseCakeShop.products: #1932 - Table 'cheesecakeshop.products' doesn't exist in engine
+-- Error reading data for table CheeseCakeShop.products: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `CheeseCakeShop`.`products`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping`
+--
+-- Error reading structure for table CheeseCakeShop.shipping: #1932 - Table 'cheesecakeshop.shipping' doesn't exist in engine
+-- Error reading data for table CheeseCakeShop.shipping: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `CheeseCakeShop`.`shipping`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcategories`
+--
+-- Error reading structure for table CheeseCakeShop.subcategories: #1932 - Table 'cheesecakeshop.subcategories' doesn't exist in engine
+-- Error reading data for table CheeseCakeShop.subcategories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `CheeseCakeShop`.`subcategories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table CheeseCakeShop.users: #1932 - Table 'cheesecakeshop.users' doesn't exist in engine
+-- Error reading data for table CheeseCakeShop.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `CheeseCakeShop`.`users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist_items`
+--
+-- Error reading structure for table CheeseCakeShop.wishlist_items: #1932 - Table 'cheesecakeshop.wishlist_items' doesn't exist in engine
+-- Error reading data for table CheeseCakeShop.wishlist_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `CheeseCakeShop`.`wishlist_items`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
